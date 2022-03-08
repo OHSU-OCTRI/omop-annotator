d17e3289ce0535ade17bd1c7394ca8eca6239d74
Amy Yates <yateam@ohsu.edu>
2022-03-08 12:57:57 -0800

    Remove webjar dependency, upgrade node, and remove comments.

--- src/main/resources/frontend/judgment.js
+++ src/main/resources/frontend/judgment.js
@@ -1,12 +1,7 @@
 import $ from 'jquery';
-//import Vue from 'vue';
-//import Counter from './components/Counter';
+import Vue from 'vue';
+import Counter from './components/Counter';
 
-console.log('This is only reached if the Vue import is commented out.');
-const el = $('#counter');
-console.log(el);
-
-/*
 new Vue({
 	el: '#counter',
 	  
@@ -14,4 +9,4 @@ new Vue({
 		return createElement(Counter);
 	}
 });
-*/
+

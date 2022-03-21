4c4ef30fc6b7acdc3ff909ea7f38e93353664361
Amy Yates <yateam@ohsu.edu>
2022-03-21 10:53:11 -0700

    Organize imports.

--- src/main/java/org/octri/omop_annotator/domain/app/AnnotationSchema.java
+++ src/main/java/org/octri/omop_annotator/domain/app/AnnotationSchema.java
@@ -1,16 +1,9 @@
 package org.octri.omop_annotator.domain.app;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 import org.octri.omop_annotator.view.Labelled;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * Represents a collection of annotation labels used when judging entries.

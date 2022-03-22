a22efc495816e7458fab13f5ff38e0053cb2ea4a
Amy Yates <yateam@ohsu.edu>
2022-03-22 12:20:55 -0700

    Check for null patient id

--- src/main/resources/frontend/visit-list.js
+++ src/main/resources/frontend/visit-list.js
@@ -4,6 +4,6 @@ import VisitList from './components/VisitList';
 const dataset = document.querySelector("#visit-list").dataset;
 const app = createApp(VisitList, { 
     contextPath: dataset.contextPath,
-    personId: Number.parseInt(dataset.personId)
+    personId: dataset.personId ? Number.parseInt(dataset.personId) : null
 });
 app.mount('#visit-list');

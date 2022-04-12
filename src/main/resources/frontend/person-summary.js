ff87fff507659ed23a382b1991a51e71b713c53d
Heath Harrelson <harrelst@ohsu.edu>
2022-04-11 17:17:09 -0700

    OA-100 Delete Obsolete JS Files
    Delete obsolete Webpack entry point scripts.

--- src/main/resources/frontend/person-summary.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import PersonSummary from './components/PersonSummary';
-
-const dataset = document.querySelector('#person_summary').dataset;
-const app = createApp(PersonSummary, {
-  contextPath: dataset.contextPath,
-  personId: dataset.personId ? Number.parseInt(dataset.personId) : null
-});
-app.mount('#person_summary');

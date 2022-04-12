4c895322bcd19bf817ac8c400cca35dafef53b56
Heath Harrelson <harrelst@ohsu.edu>
2022-04-12 14:38:28 -0700

    OA-100 Tabbed Interface
    Convert the OMOP browser from an accordion to a tabbed interface.

--- src/main/resources/frontend/components/OmopBrowser.vue
+++ src/main/resources/frontend/components/OmopBrowser.vue
@@ -1,153 +1,119 @@
 <template>
   <PersonSummary :context-path="contextPath" :person-id="personId" />
 
-  <div class="accordion" id="person_data">
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="visit_heading">
-        <button
-          class="accordion-button"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse_visits"
-          aria-expanded="true"
-          aria-controls="collapse_visits"
-        >
-          Visits
-        </button>
-      </h2>
-      <div
-        id="collapse_visits"
-        class="accordion-collapse collapse show"
-        aria-labelledby="visit_heading"
-        data-bs-parent="#person_data"
-      >
-        <div class="accordion-body">
-          <VisitList
-            class="mb-4"
-            :context-path="contextPath"
-            :person-id="personId"
-            :show-header="false"
-          />
-        </div>
-      </div>
-    </div>
+  <VisitList
+    class="mb-4"
+    :context-path="contextPath"
+    :person-id="personId"
+    :show-header="false"
+  />
 
-    <div class="accordion-item">
-      <h2 id="conditions_heading" class="accordion-header">
-        <button
-          class="accordion-button collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse_conditions"
-          aria-expanded="false"
-          aria-controls="collapse_conditions"
-        >
-          Condition Occurrences
-        </button>
-      </h2>
-      <div
-        id="collapse_conditions"
-        class="accordion-collapse collapse"
-        aria-labelledby="conditions_heading"
-        data-bs-parent="#person_data"
+  <ul class="nav nav-tabs mb-3" role="tablist">
+    <li class="nav-item" role="presentation">
+      <button
+        id="condition_occurrences_tab"
+        class="nav-link active"
+        type="button"
+        role="tab"
+        data-bs-toggle="tab"
+        data-bs-target="#condition_occurrences"
+        aria-controls="condition_occurrences"
+        aria-selected="true"
       >
-        <div class="accordion-body">
-          <div>
-            <ConditionList
-              :context-path="contextPath"
-              :person-id="personId"
-              :show-header="false"
-            />
-          </div>
-        </div>
-      </div>
+        Condition Occurrences
+      </button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button
+        id="observations_tab"
+        class="nav-link"
+        type="button"
+        role="tab"
+        data-bs-toggle="tab"
+        data-bs-target="#observations"
+        aria-controls="observations"
+        aria-selected="false"
+      >
+        Observations
+      </button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button
+        id="procedures_tab"
+        class="nav-link"
+        type="button"
+        role="tab"
+        data-bs-toggle="tab"
+        data-bs-target="#procedures"
+        aria-controls="procedures"
+        aria-selected="false"
+      >
+        Procedures
+      </button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button
+        id="measurements_tab"
+        class="nav-link"
+        type="button"
+        role="tab"
+        data-bs-toggle="tab"
+        data-bs-target="#measurements"
+        aria-controls="measurements"
+        aria-selected="false"
+      >
+        Measurements
+      </button>
+    </li>
+  </ul>
+  <div class="tab-content">
+    <div
+      id="condition_occurrences"
+      class="tab-pane fade active show"
+      role="tabpanel"
+      aria-labelledby="condition_occurrences_tab"
+    >
+      <ConditionList
+        :context-path="contextPath"
+        :person-id="personId"
+        :show-header="false"
+      />
     </div>
-
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="observations_heading">
-        <button
-          class="accordion-button collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse_observations"
-          aria-expanded="false"
-          aria-controls="collapse_observations"
-        >
-          Observations
-        </button>
-      </h2>
-      <div
-        id="collapse_observations"
-        class="accordion-collapse collapse"
-        aria-labelledby="observations_heading"
-        data-bs-parent="#person_data"
-      >
-        <div class="accordion-body">
-          <ObservationList
-            :context-path="contextPath"
-            :person-id="personId"
-            :show-header="true"
-          />
-        </div>
-      </div>
+    <div
+      id="observations"
+      class="tab-pane fade"
+      role="tabpanel"
+      aria-labelledby="observations_tab"
+    >
+      <ObservationList
+        :context-path="contextPath"
+        :person-id="personId"
+        :show-header="false"
+      />
     </div>
-
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="procedures_heading">
-        <button
-          class="accordion-button collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse_procedures"
-          aria-expanded="false"
-          aria-controls="collapse_procedures"
-        >
-          Procedures
-        </button>
-      </h2>
-      <div
-        id="collapse_procedures"
-        class="accordion-collapse collapse"
-        aria-labelledby="procedures_heading"
-        data-bs-parent="#person_data"
-      >
-        <div class="accordion-body">
-          <ProcedureList
-            :context-path="contextPath"
-            :person-id="personId"
-            :show-header="true"
-          />
-        </div>
-      </div>
+    <div
+      id="procedures"
+      class="tab-pane fade"
+      role="tabpanel"
+      aria-labelledby="procedures_tab"
+    >
+      <ProcedureList
+        :context-path="contextPath"
+        :person-id="personId"
+        :show-header="false"
+      />
     </div>
-
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="measurements_heading">
-        <button
-          class="accordion-button collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse_measurements"
-          aria-expanded="false"
-          aria-controls="collapse_measurements"
-        >
-          Measurements
-        </button>
-      </h2>
-      <div
-        id="collapse_measurements"
-        class="accordion-collapse collapse"
-        aria-labelledby="measurements_heading"
-        data-bs-parent="#person_data"
-      >
-        <div class="accordion-body">
-          <MeasurementList
-            :context-path="contextPath"
-            :person-id="personId"
-            :show-header="true"
-          />
-        </div>
-      </div>
+    <div
+      id="measurements"
+      class="tab-pane fade"
+      role="tabpanel"
+      aria-labelledby="measurements_tab"
+    >
+      <MeasurementList
+        :context-path="contextPath"
+        :person-id="personId"
+        :show-header="false"
+      />
     </div>
   </div>
 </template>

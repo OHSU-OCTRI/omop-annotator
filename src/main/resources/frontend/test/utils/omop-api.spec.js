3c4f67f696f9275615ff03bf360c41882f8bcbe2
Heath Harrelson <harrelst@ohsu.edu>
2022-04-13 17:32:20 -0700

    OA-100 OMOP API Wrapper
    Implement a simple API wrapper for our OMOP endpoints.

---
+++ src/main/resources/frontend/test/utils/omop-api.spec.js
@@ -0,0 +1,68 @@
+import OmopApi from '@/utils/omop-api';
+
+describe('OMOP API', () => {
+  const expectedPersonPrefix = '/data/person/summary';
+  const api = new OmopApi();
+
+  it('defaults to a blank URL prefix', () => {
+    expect(api.personPrefix).toEqual(expectedPersonPrefix);
+  });
+
+  it('adds the URL prefix to URLs', () => {
+    const customPrefix = '/omop_annotator';
+    const api = new OmopApi(customPrefix);
+    expect(api.personPrefix).toEqual(customPrefix + expectedPersonPrefix);
+  });
+
+  it('makes fetch requests', () => {
+    const okResponse = new Response(JSON.stringify({}), {
+      status: 200,
+      statusText: 'OK'
+    });
+
+    spyOn(window, 'fetch').and.resolveTo(okResponse);
+
+    api.getPerson(42);
+    expect(window.fetch).toHaveBeenCalledWith(`${expectedPersonPrefix}/42`, {
+      credentials: 'same-origin'
+    });
+  });
+
+  it('rejects when requests are not successful', async () => {
+    const failedResponse = new Response('', {
+      status: 401,
+      statusText: 'Unauthorized'
+    });
+
+    spyOn(window, 'fetch').and.resolveTo(failedResponse);
+
+    try {
+      await api.getPerson(42);
+      expect(false).toBe(true).withContext('request should have thrown an error');
+    } catch (e) {
+      expect(e.message).toEqual('Unauthorized');
+    }
+  });
+
+  it('extracts and returns JSON data from successful requests', async () => {
+    const person = {
+      id: 12345,
+      monthOfBirth: 1,
+      yearOfBirth: 1990,
+      age: 22,
+      gender: 'Male',
+      race: 'White',
+      ethnicity: 'Non-Hispanic'
+    };
+
+    const okResponse = new Response(JSON.stringify(person), {
+      status: 200,
+      statusText: 'OK'
+    });
+
+    spyOn(window, 'fetch').and.resolveTo(okResponse);
+
+    const response = await api.getPerson(12345);
+    expect(response).toEqual(person);
+  });
+});

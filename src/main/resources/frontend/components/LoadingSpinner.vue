977d08344fe7f52b455f7757467c2d80f3246f05
Heath Harrelson <harrelst@ohsu.edu>
2022-04-26 13:37:36 -0700

    OA-102 Format JavaScript
    Format JavaScript and Vue files.

--- src/main/resources/frontend/components/LoadingSpinner.vue
+++ src/main/resources/frontend/components/LoadingSpinner.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-      <span class="ms-2"></span>
-      <span
-        class="spinner-border spinner-border-sm text-secondary ms-auto"
-        role="status"
-        aria-hidden="true"
-      >
-      </span>
-      <span class="ms-2">Loading...</span>
-    </div>
+  <div>
+    <span class="ms-2"></span>
+    <span
+      class="spinner-border spinner-border-sm text-secondary ms-auto"
+      role="status"
+      aria-hidden="true"
+    >
+    </span>
+    <span class="ms-2">Loading...</span>
+  </div>
 </template>

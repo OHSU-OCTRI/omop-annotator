775a3a699fc0d6053f76fe48e84ce2a9d2c0c11b
Amy Yates <yateam@ohsu.edu>
2022-05-09 11:40:37 -0700

    Add test and make general test improvements.

--- src/main/resources/frontend/test/helpers.js
+++ src/main/resources/frontend/test/helpers.js
@@ -12,3 +12,19 @@ export function mockFetchResponse(jsonData, options = defaultOptions) {
   const responseData = typeof jsonData === 'string' ? jsonData : JSON.stringify(jsonData);
   return new Response(responseData, options);
 }
+
+/**
+ * Sometimes the built-in isVisible function for Vue tests does not work
+ * @param {*} wrapper
+ */
+export function isVisible(wrapper) {
+  const styleAttr = wrapper.attributes('style');
+  const classAttr = wrapper.attributes('class');
+  if (styleAttr !== undefined && styleAttr.includes('display: none;')) {
+    return false;
+  } else if (classAttr !== undefined && classAttr.includes('d-none')) {
+    return false;
+  }
+
+  return true;
+}

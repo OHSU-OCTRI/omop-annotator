2bf5b7e2b9d3562f9b21e3b58f4ad9ea7210bb0a
Amy Yates <yateam@ohsu.edu>
2022-05-13 14:04:36 -0700

    OA-111: Streamline the custom type implementation
    - Remove custom Java Descriptors that already have an implementation in Hibernate
    - Fall back on the default behavior when customization isn't needed

--- src/main/java/org/octri/omop_annotator/hibernate/ToTextTypeBuilder.java
+++ src/main/java/org/octri/omop_annotator/hibernate/ToTextTypeBuilder.java
@@ -1,24 +1,27 @@
 package org.octri.omop_annotator.hibernate;
 
 import org.hibernate.type.AbstractSingleColumnStandardBasicType;
+import org.hibernate.type.descriptor.java.StringTypeDescriptor;
 import org.hibernate.type.descriptor.sql.ClobTypeDescriptor;
-import org.hibernate.type.descriptor.sql.VarcharTypeDescriptor;
+import org.hibernate.type.descriptor.sql.LongVarcharTypeDescriptor;
 
 /**
- * This custom type builder can be used to convert a text field in a Hibernate entity to a Java String. It supports
- * Oracle Clob and Postgres Text. The out of the box Hibernate types require the @Lob annotation for Oracle.
+ * This custom type builder can be used to convert a field in the database to a Java String. If the sql field is a
+ * String, it goes through the normal Hibernate mapping. If the field is a Clob, the out of the box Hibernate descriptor
+ * requires the @Lob annotation conflicting with other db implementations, so this will convert them to a String without
+ * requiring the annotation.
  */
 public class ToTextTypeBuilder {
 
     private static final String NAME = "ToText";
 
+    private static final String DEFAULT_DATABASE = "default";
     private static final String ORACLE_DATABASE = "oracle";
-    private static final String POSTGRES_DATABASE = "postgres";
 
+    public static final AbstractSingleColumnStandardBasicType<String> DEFAULT_INSTANCE = new ToTextTypeBuilder(
+            DEFAULT_DATABASE).build();
     public static final AbstractSingleColumnStandardBasicType<String> ORACLE_INSTANCE = new ToTextTypeBuilder(
             ORACLE_DATABASE).build();
-    public static final AbstractSingleColumnStandardBasicType<String> POSTGRES_INSTANCE = new ToTextTypeBuilder(
-            POSTGRES_DATABASE).build();
 
     String database;
 
@@ -27,16 +30,28 @@ public class ToTextTypeBuilder {
     }
 
     private AbstractSingleColumnStandardBasicType<String> build() {
-        if (database.equals(POSTGRES_DATABASE)) {
-            return new PostgresTextType();
-        } else if (database.equals(ORACLE_DATABASE)) {
+        if (database.equals(ORACLE_DATABASE)) {
             return new OracleTextType();
         } else {
-            throw new IllegalArgumentException("Database " + database + " not supported");
+            return new DefaultTextType();
         }
 
     }
 
+    // Use the same Hibernate transformation given by the TextType class
+    private class DefaultTextType extends AbstractSingleColumnStandardBasicType<String> {
+
+        @Override
+        public String getName() {
+            return NAME;
+        }
+
+        public DefaultTextType() {
+            super(LongVarcharTypeDescriptor.INSTANCE, StringTypeDescriptor.INSTANCE);
+        }
+    }
+
+    // Map CLOBs to the built-in StringTypeDescriptor that can already handle them
     private class OracleTextType extends AbstractSingleColumnStandardBasicType<String> {
 
         @Override
@@ -45,21 +60,9 @@ public class ToTextTypeBuilder {
         }
 
         public OracleTextType() {
-            super(ClobTypeDescriptor.DEFAULT, ClobStringJavaDescriptor.INSTANCE);
+            super(ClobTypeDescriptor.DEFAULT, StringTypeDescriptor.INSTANCE);
         }
 
-    }
-
-    private class PostgresTextType extends AbstractSingleColumnStandardBasicType<String> {
-
-        @Override
-        public String getName() {
-            return NAME;
-        }
-
-        public PostgresTextType() {
-            super(VarcharTypeDescriptor.INSTANCE, StringStringJavaDescriptor.INSTANCE);
-        }
     }
 
 }

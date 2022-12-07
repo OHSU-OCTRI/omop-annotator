90179f7ef323c8e62a5f462f82dd56dbde10d476
Heath Harrelson <harrelst@ohsu.edu>
2022-12-07 15:40:40 -0800

    Add no-var to ESLint Config
    Add the `no-var` rule to the default ESLint config to encourage
    using `let` and `const` for variable declarations.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
   rules: {
     eqeqeq: ['error', 'always'],
     'no-console': ['error', { allow: ['warn', 'error'] }],
-    'no-unused-vars': ['error', { vars: 'all', args: 'none' }]
+    'no-unused-vars': ['error', { vars: 'all', args: 'none' }],
+    'no-var': ['error']
   },
   overrides: [
     // node files

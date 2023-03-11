77d153eade8b9dfe01bfb7933142bcfef11ccffe
lawhead <lawhead@ohsu.edu>
2023-03-10 17:30:36 -0800

    Refactor to run MassIndexer in batches

--- src/main/java/org/octri/omop_annotator/search/Indexer.java
+++ src/main/java/org/octri/omop_annotator/search/Indexer.java
@@ -1,10 +1,13 @@
 package org.octri.omop_annotator.search;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,6 +17,7 @@ import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.octri.omop_annotator.domain.omop.VisitOccurrence;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 /**
  * Component responsible for indexing existing database data.
@@ -24,6 +28,10 @@ public class Indexer {
 
     private static final Log log = LogFactory.getLog(Indexer.class);
     private EntityManager entityManager;
+
+    // Consider using configuration for this, since it is dictated by the underlying database.
+    private static final Integer MAX_BATCH_SIZE = 999;
+    private static final Integer BATCH_SIZE = 500;
 
     /**
      * Constructor
@@ -49,28 +57,49 @@ public class Indexer {
     public void indexPersistedData(List<Integer> personIds) throws IndexException {
 
         log.info("Starting OMOP data mass indexing.");
+        List<List<Integer>> subSets = ListUtils.partition(personIds, BATCH_SIZE);
+        List<MassIndexer> indexers = new ArrayList<MassIndexer>();
+        var purge = true;
+        for (List<Integer> ids : subSets) {
+            indexers.add(createIndexer(ids, purge));
+            purge = false;
+        }
+
+        runIndexers(indexers.iterator(), 1);
+    }
+
+    private void runIndexers(Iterator<MassIndexer> indexers, Integer counter) {
+        if (indexers.hasNext()) {
+            indexers.next()
+                    .start()
+                    .thenRun(() -> {
+                        log.info("Indexer " + counter + " completed!");
+                        runIndexers(indexers, counter + 1);
+                    })
+                    .exceptionally(throwable -> {
+                        log.error("Mass indexing failed!", throwable);
+                        return null;
+                    });
+        }
+    }
+
+    private MassIndexer createIndexer(List<Integer> personIds, Boolean purge) {
+        Assert.notNull(personIds, "personIds required");
+        Assert.isTrue(personIds.size() < MAX_BATCH_SIZE, "MAX_BATCH_SIZE exceeded");
         SearchSession searchSession = Search.session(entityManager);
 
         MassIndexer indexer = searchSession.massIndexer();
 
-        if (personIds != null) {
-            String cond = "e.person.id in (" + StringUtils.join(personIds, ",") + ")";
-            indexer.type(VisitOccurrence.class).reindexOnly(cond);
-        }
+        String cond = "e.person.id in (" + StringUtils.join(personIds, ",") + ")";
+        indexer.type(VisitOccurrence.class).reindexOnly(cond);
 
-        // TODO: use configuration for tuning parameters
+        // Consider using configuration for tuning parameters
         // https://docs.jboss.org/hibernate/search/6.1/reference/en-US/html_single/#mapper-orm-indexing-massindexer-parameters
         indexer
                 .idFetchSize(150)
                 .batchSizeToLoadObjects(25)
                 .threadsToLoadObjects(4)
-                .start()
-                .thenRun(() -> {
-                    log.info("Mass indexing succeeded!");
-                })
-                .exceptionally(throwable -> {
-                    log.error("Mass indexing failed!", throwable);
-                    return null;
-                });
+                .purgeAllOnStart(false);
+        return indexer;
     }
 }
